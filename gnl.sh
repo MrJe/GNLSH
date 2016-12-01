01784c0256430f2cf639421fba607dd83b1d3e2f
jmichaud <jmichaud@student.42.fr>
2016-12-01 16:56:48 +0100

    merci au revoir

--- gnl.sh
+++ gnl.sh
@@ -7,21 +7,23 @@ BLACK="\033[30m"
 RED="\033[31m"
 GREEN="\033[32m"
 YELLOW="\033[33m"
-BLUE="\033[1;34m"
+BBLUE="\033[1;34m"
+BLUE="\033[0;34m"
 PURPLE="\033[35m"
 CYAN="\033[36m"
 WHITE="\033[37m"
 
 ptitle()
 {
-	echo "${BLUE}${1}\n---------------------${DEFAULT}"
+	echo "${BBLUE}${1}${DEFAULT}"
+	echo "${BBLUE}---------------------${DEFAULT}"
 }
 
 t_btest()
 {
 	NAME=HelloWorld_25
-	clang -Wall -Wextra -Werror -I libft/includes -o mainb.o -c mainb.c
-	clang -o test_gnl mainb.o get_next_line.o -I libft/includes -L libft/ -lft
+	clang -Wall -Wextra -Werror -I libft/includes -o srcs/mainb.o -c srcs/mainb.c
+	clang -o test_gnl srcs/mainb.o get_next_line.o -I libft/includes -L libft/ -lft
 	./test_gnl ./test/t_${1}.txt > ./result/t_${1}.output
 	DIFF=$(diff ./result/t_${1}.output ./result/r_${1}.txt)
 	if [ "$DIFF" != "" ]
@@ -34,6 +36,7 @@ t_btest()
 
 btest()
 {
+	ptitle "Basics tests"
 	t_btest HelloWorld_25
 	t_btest alonglongline
 	t_btest empty
@@ -47,8 +50,8 @@ btest()
 t_1test()
 {
 	ptitle "Multi fd simple test"
-	clang -Wall -Wextra -Werror -I libft/includes -o main1.o -c main1.c
-	clang -o test_gnl main1.o get_next_line.o -I libft/includes -L libft/ -lft
+	clang -Wall -Wextra -Werror -I libft/includes -o srcs/main1.o -c srcs/main1.c
+	clang -o test_gnl srcs/main1.o get_next_line.o -I libft/includes -L libft/ -lft
 	./test_gnl ./test/11 ./test/12 ./test/13 > ./result/1test.output
 	DIFF=$(diff ./result/1test.output ./result/r_1test.txt)
 	if [ "$DIFF" != "" ]
@@ -62,8 +65,8 @@ t_1test()
 t_2test()
 {
 	ptitle "Multi fd hard test"
-	clang -Wall -Wextra -Werror -I libft/includes -o main2.o -c main2.c
-	clang -o test_gnl main2.o get_next_line.o -I libft/includes -L libft/ -lft
+	clang -Wall -Wextra -Werror -I libft/includes -o srcs/main2.o -c srcs/main2.c
+	clang -o test_gnl srcs/main2.o get_next_line.o -I libft/includes -L libft/ -lft
 	./test_gnl ./test/21 ./test/22 ./test/23 > ./result/2test.output
 	DIFF=$(diff ./result/2test.output ./result/r_2test.txt)
 	if [ "$DIFF" != "" ]
@@ -78,8 +81,8 @@ if [ $# -eq 0 ]
 then
 	make -C libft/ fclean && make -C libft/
 	clang -Wall -Wextra -Werror -I libft/includes -o get_next_line.o -c get_next_line.c
-	clang -Wall -Wextra -Werror -I libft/includes -o main.o -c main.c
-	clang -o test_gnl main.o get_next_line.o -I libft/includes -L libft/ -lft
+	clang -Wall -Wextra -Werror -I libft/includes -o srcs/main.o -c srcs/main.c
+	clang -o test_gnl srcs/main.o get_next_line.o -I libft/includes -L libft/ -lft
 elif [ $1 == "clean" ]
 then
 	make -C libft/ fclean
@@ -88,7 +91,6 @@ then
 	rm -f test_gnl
 elif [ $1 == "btest" ]
 then
-	ptitle "Basics tests"
 	btest
 elif [ $1 == "1test" ]
 then
@@ -98,7 +100,6 @@ then
 	t_2test
 elif [ $1 == "all" ]
 then
-	ptitle "Basics tests"
 	btest
 	t_1test
 	t_2test
