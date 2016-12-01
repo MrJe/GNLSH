9f58ce743fd195302d145113b45e73992dfd2133
jmichaud <jmichaud@student.42.fr>
2016-12-01 16:39:49 +0100

    git ignore + encore plus de beaute

--- gnl.sh
+++ gnl.sh
@@ -7,16 +7,14 @@ BLACK="\033[30m"
 RED="\033[31m"
 GREEN="\033[32m"
 YELLOW="\033[33m"
-BLUE="\033[34m"
+BLUE="\033[1;34m"
 PURPLE="\033[35m"
 CYAN="\033[36m"
 WHITE="\033[37m"
 
-next()
+ptitle()
 {
-	echo "###################################"
-	echo "############# N E X T #############"
-	echo "###################################"
+	echo "${BLUE}${1}\n---------------------${DEFAULT}"
 }
 
 t_btest()
@@ -34,28 +32,6 @@ t_btest()
 	fi
 }
 
-t_1test()
-{
-	clang -Wall -Wextra -Werror -I libft/includes -o main1.o -c main1.c
-	clang -o test_gnl main1.o get_next_line.o -I libft/includes -L libft/ -lft
-	./test_gnl ./test/11 ./test/12 ./test/13 > ./result/1test.output
-	echo "- - - E C H O - . O U T P U T - - -"
-	cat ./result/1test.output
-	echo "- - - - - - - D I F F - - - - - - -"
-	diff ./result/1test.output ./result/r_1test.txt
-}
-
-t_2test()
-{
-	clang -Wall -Wextra -Werror -I libft/includes -o main2.o -c main2.c
-	clang -o test_gnl main2.o get_next_line.o -I libft/includes -L libft/ -lft
-	./test_gnl ./test/21 ./test/22 ./test/23 > ./result/2test.output
-	echo "- - - E C H O - . O U T P U T - - -"
-	cat ./result/2test.output
-	echo "- - - - - - - D I F F - - - - - - -"
-	diff ./result/2test.output ./result/r_2test.txt
-}
-
 btest()
 {
 	t_btest HelloWorld_25
@@ -66,6 +42,36 @@ btest()
 	t_btest oneline
 	t_btest simple
 	t_btest text
+}
+
+t_1test()
+{
+	ptitle "Multi fd simple test"
+	clang -Wall -Wextra -Werror -I libft/includes -o main1.o -c main1.c
+	clang -o test_gnl main1.o get_next_line.o -I libft/includes -L libft/ -lft
+	./test_gnl ./test/11 ./test/12 ./test/13 > ./result/1test.output
+	DIFF=$(diff ./result/1test.output ./result/r_1test.txt)
+	if [ "$DIFF" != "" ]
+	then
+		printf "1test\033[20G ${RED}✗${DEFAULT}\n"
+	else
+		printf "1test\033[20G ${GREEN}✓${DEFAULT}\n"
+	fi
+}
+
+t_2test()
+{
+	ptitle "Multi fd hard test"
+	clang -Wall -Wextra -Werror -I libft/includes -o main2.o -c main2.c
+	clang -o test_gnl main2.o get_next_line.o -I libft/includes -L libft/ -lft
+	./test_gnl ./test/21 ./test/22 ./test/23 > ./result/2test.output
+	DIFF=$(diff ./result/2test.output ./result/r_2test.txt)
+	if [ "$DIFF" != "" ]
+	then
+		printf "2test\033[20G ${RED}✗${DEFAULT}\n"
+	else
+		printf "2test\033[20G ${GREEN}✓${DEFAULT}\n"
+	fi
 }
 
 if [ $# -eq 0 ]
@@ -82,6 +88,7 @@ then
 	rm -f test_gnl
 elif [ $1 == "btest" ]
 then
+	ptitle "Basics tests"
 	btest
 elif [ $1 == "1test" ]
 then
@@ -91,9 +98,8 @@ then
 	t_2test
 elif [ $1 == "all" ]
 then
+	ptitle "Basics tests"
 	btest
-	next
 	t_1test
-	next
 	t_2test
 fi
